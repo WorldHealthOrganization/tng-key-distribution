3243e38b063b4722d552418799a80a66c3096ee9
Karsten Räth <karsten.raeth@t-systems.com>
2023-09-27 12:15:15 +0200

    owasp failBuildOnCVSS set to 8

--- src/test/java/tng/trustnetwork/keydistribution/testdata/TrustedIssuerTestHelper.java
+++ src/test/java/tng/trustnetwork/keydistribution/testdata/TrustedIssuerTestHelper.java
@@ -1,6 +1,6 @@
 /*-
  * ---license-start
- * eu-digital-green-certificates / dgca-verifier-service
+ * WorldHealthOrganization / tng-key-distribution
  * ---
  * Copyright (C) 2021 T-Systems International GmbH and all other contributors
  * ---
