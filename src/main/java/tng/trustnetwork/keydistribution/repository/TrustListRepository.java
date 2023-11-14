cfaccf1aafc4722f790ea32d4147b358a16ec47a
Jyothsna-Dass <Jyothsna.Dass@t-systems.com>
2023-11-14 18:11:36 +0100

    adding modified java code for DDCCGW-423 and removing java classes which are part of DDCCGW-420(this will be added in another feature branch)

--- src/main/java/tng/trustnetwork/keydistribution/repository/TrustListRepository.java
+++
@@ -1,7 +0,0 @@
-package tng.trustnetwork.keydistribution.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tng.trustnetwork.keydistribution.entity.TrustedIssuerEntity;
-
-public interface TrustListRepository extends JpaRepository<TrustedIssuerEntity, Long> {
-}
--- src/main/java/tng/trustnetwork/keydistribution/service/JwkServiceImpl.java
+++ src/main/java/tng/trustnetwork/keydistribution/service/JwkServiceImpl.java
@@ -1,59 +1,24 @@
 package tng.trustnetwork.keydistribution.service;
 
-import java.util.Arrays;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import eu.europa.ec.dgc.gateway.connector.dto.CertificateTypeDto;
+import eu.europa.ec.dgc.gateway.connector.dto.TrustListItemDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
+import tng.trustnetwork.keydistribution.client.KdsGatewayConnectorRestClient;
 
 @Service
-@Slf4j
-public class JwkServiceImpl implements JwkService {
-    private String urldata = "http://localhost:8080/trustlist";
-    //private String getApiKey = "";
-    //private String getApiPwd = "";
-    
-    //actual gateway API to be accessed: !!!! private String urldata = "https://tng-dev.who.int/trustList";
-    
-    RestTemplate restTemplate = new RestTemplate();
+@RequiredArgsConstructor
+public class JwkService {
 
-    public ResponseEntity<String> gatewayApiCall() {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            HttpEntity<String> httpEntity  = new HttpEntity<String>("parameters", headers);
-            ResponseEntity<String> response = restTemplate.exchange(urldata, HttpMethod.GET, 
-                httpEntity, String.class);
-            return response;
-        }   catch (Exception e) {
-            log.error("error when reading", e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
-                "exception calling API endpoint",e);
-        }
+    private final  KdsGatewayConnectorRestClient kdsGatewayConnectorRestClient;
+
+    public List<TrustListItemDto> gatewayApiForAll() {
+        return kdsGatewayConnectorRestClient.getTrustedCertificates();
     }
-    
-    //    public List<TrustListItemDto> gatewayApiCall() {
-    //        try {
-    //            HttpHeaders headers = new HttpHeaders();
-    //            headers.set("Key", getApiKey);
-    //            headers.set("Pwd", getApiPwd);
-    //            ResponseEntity<List> response = restTemplate.exchange(url, HttpMethod.GET, 
-    //               new HttpEntity<>(headers), List.class);
-    //            log.info("output is ", response.getBody());    
-    //            return response.getBody();
-    //        }   catch (Exception e) {
-    //            log.error("error when reading", e);
-    //            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
-    //               "exception calling API endpoint",e);
-    //        }
-    //    }
+
+    public List<TrustListItemDto> gatewayApiForType(CertificateTypeDto type) {
+        return kdsGatewayConnectorRestClient.getTrustedCertificates(type);
+    }
+
 }
-
-
-
