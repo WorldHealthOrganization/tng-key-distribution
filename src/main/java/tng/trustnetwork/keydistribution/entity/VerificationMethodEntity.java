7aa27bbe6049d3c003d927a041203b14c38de9fd
Piyush Saini <piyush.saini@t-systems.com>
2023-12-27 11:55:54 +0100

    Entities mapping correction

--- src/main/java/tng/trustnetwork/keydistribution/entity/VerificationMethodEntity.java
+++ src/main/java/tng/trustnetwork/keydistribution/entity/VerificationMethodEntity.java
@@ -1,17 +1,12 @@
 package tng.trustnetwork.keydistribution.entity;
 
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -46,7 +41,4 @@ public class VerificationMethodEntity {
 	@JoinColumn(name = "id")
 	private PublicKeyJwkEntity publicKeyJwk;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "decentralized_identifier_id")
-    private DecentralizedIdentifierEntity decentralizedIdentifierEntity;
 }
